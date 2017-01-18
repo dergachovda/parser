56fff48c4b31d9fcf59dea1ba95d2b98958cdf8a
Dima D <dergachovda@gmail.com>
2017-01-18 18:26:46 +0200

    add geo

---
+++ src/main/java/ua/org/ecity/Test.java
@@ -0,0 +1,14 @@
+package ua.org.ecity;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "V";
+        int i = 0;
+        try {
+            i = Integer.parseInt(str);
+        } catch (Exception e) {
+
+        }
+        System.out.println(i);
+    }
+}
--- src/main/java/ua/org/ecity/parser.java
+++ src/main/java/ua/org/ecity/parser.java
@@ -3,7 +3,6 @@ package ua.org.ecity;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -15,6 +14,7 @@ public class parser {
 
     private static final String REGION = "Область";
     private static final String ESTABLISHMENT = "Основан";
+    private static final String ESTABLISHMENT_2 = "Первое упоминание";
     private static final String POPULATION = "Население";
     private static final String GPS = "Координаты";
 
@@ -28,9 +28,9 @@ public class parser {
         List<String> cities = Arrays.asList(
                 "Авдеевка",
                 "Александрия_(Кировоградская_область)",
-                "Александровск",
+                "Александровск_(Луганская_область)",
                 "Алёшки",
-                "Алмазная"
+                "Березно"
         );
 
         for (String city : cities) {
@@ -47,11 +47,19 @@ public class parser {
 
             Elements vcard = doc.select("table.infobox.vcard");
             Elements tr = vcard.select("tr");
+            Elements geo = tr.select("span.geo");
+            String sGeo = geo.toString();
+            sGeo = sGeo.substring(18 , sGeo.indexOf("</span>"));
+            System.out.println(sGeo);
+            /*
             for (Element el : tr) {
-                String[] strs = el.text().split(" ", 2);
+//                String[] strs = el.text().split(" ", 2);
+//                if (strs[0].equals(REGION)) {
+//                System.out.println(el.text());
 
-                if (strs[0].equals(REGION)) {
-//                    System.out.println(Arrays.toString(strs));
+                if (el.text().startsWith(REGION)) {
+                    String[] strs = el.text().split(" ", 2);
+                    System.out.println(Arrays.toString(strs));
                     region = strs[1].split(" ")[0];
                     int index = region.indexOf(region.substring(0, 2), 1);
                     if (index > 0) {
@@ -59,18 +67,35 @@ public class parser {
                     }
                     System.out.println(REGION + ": " + region);
                 }
-                if (strs[0].equals(ESTABLISHMENT)) {
+//                if (strs[0].equals(ESTABLISHMENT) || strs[0].equals(ESTABLISHMENT_2)) {
+                if (el.text().startsWith(ESTABLISHMENT)) {
+                    String[] strs = el.text().split(" ", 2);
+                    System.out.println(Arrays.toString(strs));
                     establishment = strs[1];
                     System.out.println(ESTABLISHMENT + ": " + establishment);
                 }
-                if (strs[0].equals(POPULATION)) {
-                    String[] s = strs[1].split("\\[")[0].split("\\D+");
+
+                if (el.text().startsWith(ESTABLISHMENT_2)) {
+                    String[] strs = el.text().split(" ");
+                    System.out.println(Arrays.toString(strs));
+                    if (establishment.isEmpty()) {
+                        establishment = strs[2];
+                    }
+                    System.out.println(ESTABLISHMENT + ": " + establishment);
+                }
+
+//                if (strs[0].equals(POPULATION)) {
+                if (el.text().startsWith(POPULATION)) {
+                    String[] strs = el.text().split(" ", 2);
+                    System.out.println(Arrays.toString(strs));
+                    String[] s = strs[1].split("\\[")[0].split("\\(")[0].split("\\D+");
                     for (String ss : s) {
                         population += ss;
                     }
                     System.out.println(POPULATION + ": " + population);
                 }
             }
+            */
         }
     }
 }
